3070b497d820939abaafa1d011d9ca7bfa6d8030
Yashika0902 <sumati0902@gmail.com>
2025-08-25 08:42:53 +0530

    Added UserDefaults

--- TooDood/Model/ToDoItem.swift
+++ TooDood/Model/ToDoItem.swift
@@ -1,14 +1,34 @@
 import Foundation
 
-struct TodoItem: Identifiable {
-    let id = UUID()
+struct TodoItem: Identifiable, Codable {
+    var id: UUID = UUID()
     var title: String
     var isCheck: Bool = false
 }
 
-
 class TodoViewModel: ObservableObject {
-    @Published var todos: [TodoItem] = [
-        
-    ]
+    @Published var todos: [TodoItem] = [] {
+        didSet {
+            saveTodos()
+        }
+    }
+    
+    private let key = "todos_list"
+    
+    init() {
+        loadTodos()
+    }
+    
+    private func saveTodos() {
+        if let data = try? JSONEncoder().encode(todos) {
+            UserDefaults.standard.set(data, forKey: key)
+        }
+    }
+    
+    private func loadTodos() {
+        if let data = UserDefaults.standard.data(forKey: key),
+           let decoded = try? JSONDecoder().decode([TodoItem].self, from: data) {
+            todos = decoded
+        }
+    }
 }
